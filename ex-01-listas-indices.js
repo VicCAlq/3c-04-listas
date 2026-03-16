aa53e73fad9efe152b2ac7e906cb25898e402302
rayRayanjoS <rayane.anjos@nave.org.br>
2026-03-16 07:41:31 -0300

    revisar

--- ex-01-listas-indices.js
+++ ex-01-listas-indices.js
@@ -141,7 +141,8 @@ Crie uma lista (com const) chamado "frutas" com os itens: "maçã", "banana", "l
 "uva", "pêra", "manga". Armazene na variável "resposta01" o segundo item dessa lista.
 */
 // Array na linha abaixo
-
+ const frutas ["maçã", "banana", "laranja", 
+"uva", "pêra", "manga"]
 // Operações necessárias e resposta a partir da linha abaixo
 export const resposta01 = false
 
